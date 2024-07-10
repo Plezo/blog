ff219dc1e52a8e2295debd479786fb4f66f4ce08
Josiel Perez <Josielp0218@gmail.com>
2024-07-10 00:52:27 -0400

    (feat) github a provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [GitHub],
 });
